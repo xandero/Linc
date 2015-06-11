c308b58aee93f58a6ca4209193c0236795ed7129
xandero <alexander.ohan@gmail.com>
2015-06-11 15:42:13 -0700

    rendering graph

--- js/planets.js
+++ js/planets.js
@@ -16,22 +16,43 @@ $(document).ready(function() {
     
     var tableData = {
       chartData: function() {
-
+        var chartData = [];
+        data.find('tbody td').each(function() {
+          chartData.push($(this).text());
+        });
+        return chartData;
       },
       chartHeading: function() {
-
+        var chartHeading = data.find('caption').text();
+        return chartHeading;
       },
       chartLegend: function() {
-
+        var chartLegend = [];
+        data.find('tbody th').each(function() {
+          chartLegend.push($(this).text());
+        });
+        return chartLegend;
       },
       chartYMax: function() {
-
+        var chartData = this.chartData();
+        var chartYMax = Math.ceil(Math.max.apply(Math, chartData) / 1000) * 1000;
+        return chartYMax;
       },
       yLegend: function() {
-
+        var chartYMax = this.chartYMax();
+        var yLegend = [];
+        var yAxisMarkings = 5;            
+        for (var i = 0; i < yAxisMarkings; i++) {
+          yLegend.unshift(((chartYMax * i) / (yAxisMarkings - 1)) / 1000);
+        }
+        return yLegend;
       },
       xLegend: function() {
-
+        var xLegend = [];
+        data.find('thead th').each(function() {
+          xLegend.push($(this).text());
+        });
+        return xLegend;
       },
       columnGroups: function() {
         columnGroup = [];
@@ -46,23 +67,60 @@ $(document).ready(function() {
       }
     }
 
-      $.each(columnGroup, function(i) {
-        var barGroup = $('<div class="bar-group"></div>');
-        for (var c = 0, d = columnGroup[i].length; c < d; c++){
-          var barObject = {};
-          barObject.label = this[c];
-          barObject.height = barObject.label/chartYMax * 100 + '%';
-          barObject.bar = $('div class="bar fig' + j +'"><span>' + barObject.label + '</span></div>').appendTo(barGroup);
-          bars.push(barObject);
-        }
-        barGroup.appendTo(barContainer);
-      });
-    function  displayGraph(bars) {
+    chartData = tableData.chartData();    
+    chartYMax = tableData.chartYMax();
+    columnGroups = tableData.columnGroups();
 
+    $.each(columnGroup, function(i) {
+      var barGroup = $('<div class="bar-group"></div>');
+      for (var c = 0, d = columnGroup[i].length; c < d; c++){
+        var barObject = {};
+        barObject.label = this[c];
+        barObject.height = barObject.label/chartYMax * 100 + '%';
+        barObject.bar = $('div class="bar fig' + j +'"><span>' + barObject.label + '</span></div>').appendTo(barGroup);
+        bars.push(barObject);
+      }
+      barGroup.appendTo(barContainer);
+    });
+
+    var chartHeading = tableData.chartHeading();
+    var heading = $('<h4>' + chartHeading + '</h4>');
+    heading.appendTo(figureContainer);
+
+    var chartLegend = tableData.chartLegend();
+    var legendList  = $('<ul class="legend"></ul>');
+    $.each(chartLegend, function(i) {     
+      var listItem = $('<li><span class="icon fig' + i + '"></div></span>' + this + '</li>')
+        .appendTo(legendList);
+      
+    });
+    legendList.appendTo(figureContainer);
+
+    var yLegend = tableData.yLegend();
+    var yAxisList = $('<ul class="y-axis"></ul>');
+    $.each(yLegend, function(i) {
+      var listItem = $('<li><span>' + this + '</span></li>').appendTo(yAxisList);
+    });
+    yAxisList.appendTo(graphContainer);
+
+    var xLegend = tableData.xLegend();
+    var xAxisList = $('<ul class="x-axis"></ul>');
+    $.each(xLegend, function(i) {
+      var listItem = $('<li><span>' + this + '</span></li>').appendTo(xAxisList);
+    });
+    xAxisList.appendTo(graphContainer);
+    
+    barContainer.appendTo(graphContainer);    
+    graphContainer.appendTo(figureContainer);
+    figureContainer.appendTo(container);
     }
-    function resetGraph() {
-      displayGraph(bars);
+
+    function  displayGraph(bars, i) {
+      if (i < bars.length) {
+        $(bars[i].bar).animate({height: bars[i].height}, 500);
+        barTimer = setTimeout(function(){i++; displayGraph(bars, i)}, 200);
+      }
     }
-    resetGraph();
+    createGraph();
   }
 });)
